0395ee5458453890c28eee0be0302b95684d2e2f
enricodias <enricodias@gmail.com>
2021-07-18 15:06:07 -0300

    fix StyleCI and increase usleep for tests

--- tests/MouseApiTest.php
+++ tests/MouseApiTest.php
@@ -132,7 +132,7 @@ class MouseApiTest extends BaseTestCase
     /**
      * @throws \HeadlessChromium\Exception\CommunicationException
      * @throws \HeadlessChromium\Exception\NoResponseAvailable
-    */
+     */
     public function testFind_withPositionOutOfBounds(): void
     {
         // initial navigation
@@ -171,7 +171,7 @@ class MouseApiTest extends BaseTestCase
      * @throws \HeadlessChromium\Exception\CommunicationException
      * @throws \HeadlessChromium\Exception\NoResponseAvailable
      * @throws \HeadlessChromium\Exception\ElementNotFoundException
-    */
+     */
     public function testFind_withMissingElement(): void
     {
         $this->expectException(\HeadlessChromium\Exception\ElementNotFoundException::class);
@@ -185,7 +185,7 @@ class MouseApiTest extends BaseTestCase
     /**
      * @throws \HeadlessChromium\Exception\CommunicationException
      * @throws \HeadlessChromium\Exception\NoResponseAvailable
-    */
+     */
     public function testGetPosition(): void
     {
         // initial navigation
@@ -195,7 +195,7 @@ class MouseApiTest extends BaseTestCase
 
         // find element with id "a"
         $page->mouse()->find('#a');
-        \usleep(400000);
+        \usleep(800000);
 
         $x = $page->mouse()->getPosition()['x'];
         $y = $page->mouse()->getPosition()['y'];
